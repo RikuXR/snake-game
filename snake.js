7c53625d6718f8f2920cdafdc0857ee6f30c8f6c
Riku <rikuxeyer@protonmail.com>
2020-04-16 13:54:00 +0600

    Loading Images

---
+++ snake.js
@@ -0,0 +1,14 @@
+const cvs = document.getElementById("snake");
+const ctx = cvs.getContext("2d");
+
+// create the unit
+const box = 32;
+
+// load images
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const foodImg = new Image();
+foodImg.src = "img/food.png";
+
+console.log(foodImg);
