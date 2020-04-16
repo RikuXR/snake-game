c17fd733176bc0c0d205ac28ddcbb376668c33e5
Riku <rikuxeyer@protonmail.com>
2020-04-16 14:15:26 +0600

    Draw Function

--- snake.js
+++ snake.js
@@ -23,3 +23,25 @@ let food = {
   x: Math.floor(Math.random() * 17 + 1) * box,
   y: Math.floor(Math.random() * 15 + 3) * box,
 };
+
+// create the score
+let score = 0;
+
+// draw everything
+function draw() {
+  ctx.drawImage(ground, 0, 0);
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "green" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+
+  ctx.drawImage(foodImg, food.x, food.y);
+
+  ctx.fillStyle = "white";
+  ctx.font = "37px Arial";
+  ctx.fillText(score, 2 * box, 1.6 * box);
+}
+
+// update canvas
+let game = setInterval(draw, 100);
